81a6e5d1113d7bfa9e627856c1277c5b3f056333
Shashank P <shashankp2832@gmail.com>
2023-03-25 16:27:17 +0530

    Added interface abstraction to translate and run. TODO by ARVIND

---
+++ ml-terminal-interface.py
@@ -0,0 +1,10 @@
+def translate_to_command(command: str):
+    command = "files starting with s"
+    return "ls | grep s*"
+
+def get_output(bash_command: str):
+    output = ""
+    
+    # TODO: Write your code here
+    
+    return output
