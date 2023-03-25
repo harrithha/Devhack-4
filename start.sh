80f66e014dd3b74242bfe335a53b3f7a116998a3
Anand Hegde <anandishegde@gmail.com>
2023-03-26 01:39:00 +0530

    changed the path at some places

--- start.sh
+++ start.sh
@@ -10,11 +10,9 @@ else
     cp $(pwd)/{intellish.py,ml_terminal_interface.py} $ne/
 fi
 
-if alias your_alias_name >/dev/null 2>&1; then 
+if alias intellish >/dev/null 2>&1; then 
     echo "alias exists"
 else 
-    echo "alias intellish='python $(pwd)/intellish.py'" >> ~/.bashrc
-    eval "$(cat ~/.bashrc | tail -n +10)"
+    echo "alias intellish='python $ne/intellish.py'" >> ~/.bashrc
     exec bash
-    alias intellish='python $(pwd)/intellish.py'
 fi
