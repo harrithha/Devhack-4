9d27df55b3c71dec5fc20ccf356840c8ddb31b75
Shashank P <shashankp2832@gmail.com>
2023-03-25 19:52:40 +0530

    - Added good heading - Changed colors heading to blue

--- intellish.py
+++ intellish.py
@@ -2,17 +2,37 @@ import os
 import subprocess
 from time import sleep
 from ml_terminal_interface import translate_to_command, run_command
+import colorama
+from colorama import Fore
 
 
 current_folder = os.path.basename(os.getcwd())
 
+def print_heading():
+    print(Fore.BLUE)
+    print("--------------------------------------------------------------------------")
+    print("|    _____ _   _ _______ ______ _      _      _____      _____ _    _    |")
+    print("|   |_   _| \ | |__   __|  ____| |    | |    |_   _|    / ____| |  | |   |")
+    print("|     | | |  \| |  | |  | |__  | |    | |      | |_____| (___ | |__| |   |")
+    print("|     | | | . ` |  | |  |  __| | |    | |      | |______\___ \|  __  |   |")
+    print("|    _| |_| |\  |  | |  | |____| |____| |____ _| |_     ____) | |  | |   |")
+    print("|   |_____|_| \_|  |_|  |______|______|______|_____|   |_____/|_|  |_|   |")
+    print("|                                                                        |")
+    print("--------------------------------------------------------------------------")
+    print(Fore.GREEN)
+    
+
 def print_intro():
+    print(Fore.BLUE)
     print("--------------------")
     print("Enter H: for help")
     print("Enter Q: to Quit")
     print("Enter C: to Clear")
     print("--------------------")
+    print(Fore.GREEN)
+    
 
+print_heading()
 print_intro()
 
 
@@ -37,5 +57,6 @@ while True:
     
     output = run_command(command)
     print(output)
+    print(Fore.GREEN)
 
 print_quit()
