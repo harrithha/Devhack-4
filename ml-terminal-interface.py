c9c9f7f01e57e7bdfe39a1c3aafc036f43c5f2e2
Arvind Kumar M <200020008@iitdh.ac.in>
2023-03-25 19:12:35 +0530

    Added function for running command and returning output

--- ml-terminal-interface.py
+++ ml-terminal-interface.py
@@ -1,10 +1,13 @@
+import subprocess
+
 def translate_to_command(command: str):
     command = "files starting with s"
     return "ls | grep s*"
 
-def get_output(bash_command: str):
-    output = ""
-    
-    # TODO: Write your code here
-    
+def run_command(command):
+    result = subprocess.run(command, stdout=subprocess.PIPE, shell=True)
+    output = result.stdout.decode().strip()
     return output
+    
+output = run_command("ls  i*")
+print(output)
