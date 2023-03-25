bf74a17139a0957a249439adba5c3813a64c6703
Shashank P <shashankp2832@gmail.com>
2023-03-25 19:36:43 +0530

    Added quit and clear for internal commands

--- intellish.py
+++ intellish.py
@@ -6,21 +6,36 @@ from ml_terminal_interface import translate_to_command, run_command
 
 current_folder = os.path.basename(os.getcwd())
 
-print("==================== INTELLI-sh ====================")
-print("Enter H: for help")
+def print_intro():
+    print("--------------------")
+    print("Enter H: for help")
+    print("Enter Q: to Quit")
+    print("Enter C: to Clear")
+    print("--------------------")
+
+print_intro()
 
 
 print()
-
-def print_help():
-    print("Enter H: for help")
-    print("Enter Q: to Quit")
+    
+def print_quit():
+    print("Bye!")
+    return
 
 while True:
     command = input(f"{current_folder}) ")
-    if command == "Q":
+    
+    if command.lower() == "h":
+        print("TODO: HELP")
+        continue
+    
+    if command.lower() == "q": 
         break
+    
+    if command.lower() == "c":
+        command = "clear"
+    
     output = run_command(command)
     print(output)
-    print()
-    
+
+print_quit()
